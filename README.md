90901a8aad0517643db6e895de47a46f61ce6d8b
izayoiwind <izayoiwind.extreme@gmail.com>
2020-02-18 00:15:05 +0900

    全体的に処理を修正、npmモジュール化

--- README.md
+++ README.md
@@ -6,8 +6,72 @@ npm install（もしくはyarn install）でインストールしたライブラ
 
 使い方
 ---
-license-checkerで出力したJSONをlicchecker.jsに読み込ませます。とりあえず整理したら書きます。
+まずは、以下のコマンドでlicense-checkerを実行し、元となるライセンス情報ファイルを作成します。
+```
+$ license-checker --json --customPath ./licenses/format.json --production --out ./licenses/licenses.json
+```
+上記のコマンドで指定しているformat.jsonは以下の内容となります。
+```
+{
+	"name": "",
+	"version": "",
+	"description": "",
+	"licenses": "",
+	"copyright": "",
+	"licenseFile": "",
+	"licenseText": "",
+	"licenseModified": ""
+}
+```
+license-checkerで出力したJSONファイルをlicchecker.jsに読み込ませます。何も指定しない場合は、実行ディレクトリにあるlicenses.jsonが対象となります。実行後に、ライセンス条文等を整理したlicenses_out.jsonが吐き出されます。ライセンスや著作権表記の確認が必要な場合は、その旨が出力されます。
+```
+$ node licchecker.js
+```
+他のnodeを使用するアプリケーションの依存ライブラリとして取得し、かつyarnを使用している場合は、以下のコマンドで実行可能です。
+```
+$ yarn licchecker
+```
+
+オプション
+---
+以下のオプションを指定できます。
+
+|オプション|パラメータ|説明|
+|---|---|---|
+|-i|ファイルパス|入力ライセンス情報ファイルを指定できます。|
+|-o|ファイルパス|出力ライセンス情報ファイルを指定できます。|
+|-k|ファイルパス|既知ライセンス情報ファイルを指定できます。|
+|--force|なし|エラーがあってもファイル出力を行います。エラーとなった分は出力対象に含まれません。|
+|--index|なし|インデックス番号を含めます。|
+|--list|なし|連想配列ではなく、リスト形式で出力します。|
+
+オプション指定例
+```
+$ node licchecker.js -i input.json -o output.json -k knownlicences.json --force --index --list
+```
+
+既知ライセンス情報ファイルについて
+---
+licenses.jsonに著作権表記が含まれていない等、警告が出る場合に、既知ライセンス情報をknown.jsonという名前で作成すると、既知のライブラリについての処理を自動化できます。既知以外のライブラリが追加され、ライセンスや著作権表記の確認が必要になった場合、または既知のライブラリでもバージョンアップ等でライセンスが変更になった場合に、警告を出すことができます。
+
+既知ライセンス情報ファイルの書き方
+---
+以下のように記載します。
+```
+{
+    "ライブラリ名（licenses.jsonのnameに記載されるもの ※1）": {
+        "licenses": "ライセンス種別 ※2",
+        "licensesOrigin": "licenses.jsonに記載のライセンス種別",
+        "copyright": "著作権情報",
+		"ignore": 出力対象としない場合はtrue、それ以外は定義しないまたはfalse,
+        "licenseText": "ライセンス条文"
+    }
+}
+```
+※1 同一ライブラリのバージョン違いが含まれ、ライセンスが異なる場合は、アンダースコアとlicensesOriginの内容を付与することが可能です。この場合、当該ライブラリの記述全てに対して同様の指定を行ってください。例：library_MIT
+
+※2 MIT、Apache-2.0、ISC、BSD-2-Clause、BSD-3-Clauseのいずれかを指定することで、licenses.jsonとの表記揺れを吸収できます。それ以外のライセンスの場合はKNOWN_LICENSEと指定します。KNOWN_LICENSEを指定した場合、条文が必要であればlicenseTextを指定してください。ただし、パブリックドメイン等、ライブラリを使用しているという明示が必要ないものであれば、"ignore": trueを指定することで、最終的に生成されるライセンス情報に掲載されないようにすることができます。この場合は、条文の定義は不要です。
 
 ライセンス
 ---
-ライセンスはMITです。ご自由にお使いください。
+ライセンスはMITです。商用利用、非商用利用問わずご自由にお使いください。
--- licchecker.js
+++ licchecker.js
@@ -1,102 +1,267 @@
+#!/usr/bin/env node
+
+/*
+ * Copyright (c) 2019-2020 Harukanagi (@izayoiwind).
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const fs = require('fs')
 const argv = process.argv
 let inputFile = './licenses.json'
 let outputFile = './licenses_out.json'
+let knownFile = './known.json'
 
-let mitTxt = ''
-let apache20Txt = ''
-let iscTxt = ''
-let bsd2clauseTxt = ''
-let bsd3clauseTxt = ''
-try {
-    mitTxt = fs.readFileSync('./lictexts/mit.txt', 'utf8')
-    apache20Txt = fs.readFileSync('./lictexts/apache20.txt', 'utf8')
-    iscTxt = fs.readFileSync('./lictexts/isc.txt', 'utf8')
-    bsd2clauseTxt = fs.readFileSync('./lictexts/bsd2clause.txt', 'utf8')
-    bsd3clauseTxt = fs.readFileSync('./lictexts/bsd3clause.txt', 'utf8')
-} catch (e) {
-    console.error('ライセンステンプレートファイルが存在しません')
-    process.exit(1)
+/** ライセンス条文 */
+licenseTemplateTextDictionary = {}
+
+/**
+ * コンソールに警告を出力する。（黄文字）
+ * @param {*} string 出力する文字列
+ */
+const outputConsoleWarning = (string) => {
+    console.log(`\u001b[33m${string}\u001b[0m`)
 }
 
-const checkArgv = (inArgv, inIndex, inCorrected) => {
+/**
+ * コンソールにエラーを出力する。（赤文字）
+ * @param {*} string 出力する文字列
+ */
+const outputConsoleError = (string) => {
+    console.log(`\u001b[31m${string}\u001b[0m`)
+}
+
+/**
+ * コマンドライン引数の-付きオプションを確認する。
+ * @param {*} inArgv コマンドライン引数の配列
+ * @param {*} inIndex オプションを検出したインデックス
+ * @returns オプションの値
+ */
+const getOptionArgv = (inArgv, inIndex) => {
     if (inArgv.length <= inIndex + 1) {
-        console.error('オプションの指定が不正です')
-        process.exit(1)
+        throw new Error()
     }
-    inCorrected()
+    return inArgv[inIndex + 1]
 }
-argv.forEach((value, index) => {
-    if (value === '-i') {
-        checkArgv(argv, index, () => {
-            inputFile = argv[index + 1]
-        })
-    } else if (value === '-o') {
-        checkArgv(argv, index, () => {
-            outputFile = argv[index + 1]
-        })
+
+/**
+ * 著作権表記のエラーメッセージを出力する。
+ * @param {*} key ライブラリのキー
+ * @param {*} licenseType ライセンス種別
+ */
+const outputCopyrightError = (key, licenseType) => {
+    console.log(`${key}：著作権表記を確認してください。（${licenseType}）`)
+}
+
+/**
+ * ライセンス要確認のエラーメッセージを出力する。
+ * @param {*} key ライブラリのキー
+ * @param {*} licenseType ライセンス種別
+ */
+const outputLicenseError = (key, licenseType) => {
+    outputConsoleError(`${key}：ライセンスを確認してください。（${licenseType}）`)
+}
+
+/**
+ * 既知ライセンス情報条文エラーのエラーメッセージを出力する。
+ * @param {*} key ライブラリのキー
+ */
+const outputKnownLicenseTextError = (key) => {
+    outputConsoleError(`${key}：既知ライセンス情報に条文がありません。出力の必要がない場合は"ignore": trueを追加してください。`)
+}
+
+/**
+ * 著作権情報から、ライセンス条文を生成する。
+ * @param {*} licTemplateFileText ライセンス条文テンプレート
+ * @param {*} copyright 著作権情報
+ */
+const generateLicenseText = (licTemplateFileText, copyright) => {
+    return !!copyright ? licTemplateFileText.replace('%%%COPYRIGHT%%%', copyright) : ''
+}
+
+/**
+ * ライセンステンプレートファイルを読み込む。
+ */
+const readLicenseTemplateFile = () => {
+    try {
+        licenseTemplateTextDictionary['MIT'] = fs.readFileSync(`${__dirname}/lictexts/mit.txt`, 'utf8')
+        licenseTemplateTextDictionary['Apache-2.0'] = fs.readFileSync(`${__dirname}/lictexts/apache20.txt`, 'utf8')
+        licenseTemplateTextDictionary['ISC'] = fs.readFileSync(`${__dirname}/lictexts/isc.txt`, 'utf8')
+        licenseTemplateTextDictionary['BSD-2-Clause'] = fs.readFileSync(`${__dirname}/lictexts/bsd2clause.txt`, 'utf8')
+        licenseTemplateTextDictionary['BSD-3-Clause'] = fs.readFileSync(`${__dirname}/lictexts/bsd3clause.txt`, 'utf8')
+    } catch (e) {
+        throw new Error('ライセンステンプレートファイルが存在しません。')
     }
-})
+}
+
+/**
+ * ライセンス情報ファイルを読み込む。
+ * @param {*} inputFile ファイル名
+ * @returns ライセンス情報
+ */
+const readLicenseFile = (inputFile) => {
+    try {
+        fs.statSync(inputFile)
+        return JSON.parse(fs.readFileSync(inputFile, 'utf8'))
+    } catch (e) {
+        throw new Error('元となるライセンス情報ファイルが存在しない、または不正です。licenses.jsonという名前で配置するか、-iオプションでファイル名を指定してください。')
+    }
+}
+
+/**
+ * 既知ライセンス情報ファイルを読み込む。
+ * @param {*} knownFile ファイル名
+ */
+const readKnownLicenseFile = (knownFile) => {
+    try {
+        fs.statSync(knownFile)
+        return JSON.parse(fs.readFileSync(knownFile, 'utf8'))
+    } catch (error) {
+        // 既知ライブラリ情報ファイルが無い場合は何もしない。  
+        return {}
+    }
+}
+
+// このフラグがtrueの場合、エラーがあったとしてもファイルの出力を行う。
+// エラーがあったものについては出力対象にならない。
+let ignoreCheckError = false
+let isList = false
+let isOutputIndex = false
+
+// コマンドラインオプション取得
 try {
-    fs.statSync(inputFile)
+    argv.forEach((value, index) => {
+        if (value === '-i') {
+            inputFile = getOptionArgv(argv, index)
+        } else if (value === '-o') {
+            outputFile = getOptionArgv(argv, index)
+        } else if (value === '-k') {
+            knownFile = getOptionArgv(argv, index)
+        } else if (value === '--force') {
+            ignoreCheckError = true
+        } else if (value === '--list') {
+            isList = true
+        } else if (value === '--index') {
+            isOutputIndex = true
+        }
+    })
 } catch (e) {
-    console.error('指定されたファイルは存在しません')
+    console.error('オプションの指定が不正です。')
     process.exit(1)
 }
-const containsCopyright = (value) => {
-    return !!value.copyright
-}
-const generateLicenseText = (licTemplateFileText, value) => {
-    return containsCopyright(value) ? licTemplateFileText.replace('%%%COPYRIGHT%%%', value.copyright) : ''
-}
-const checkLicenseText = (key, licenseType, licenseText) => {
-    if (!licenseText) {
-        console.log(`${key}：著作権表記を確認してください（${licenseType}）`)
-    }
-}
+
+/** エラーカウント */
+let errorCount = 0
+
 try {
-    const json = JSON.parse(fs.readFileSync(inputFile, 'utf8'))
-    const newLicensesJsonData = {}
-    for (key in json) {
-        let value = json[key]
-        if (!value.name || !value.version) {
-            console.log(`${key}：名前またはバージョンがありません`)
-        }
-        if (value.private) {
+    // ライセンステンプレートファイルを読み込む。
+    readLicenseTemplateFile()
+    // 既知ライブラリ情報ファイルを読み込む。
+    const knownLicensesDictionary = readKnownLicenseFile(knownFile)
+    // ライセンス情報ファイルを読み込む。
+    const inputLicensesDictionary = readLicenseFile(inputFile)
+    const newLicensesDictionary = {}
+    let index = 0
+    for (inputLicensesKey in inputLicensesDictionary) {
+        let inputLicensesData = inputLicensesDictionary[inputLicensesKey]
+        if (!inputLicensesData.name || !inputLicensesData.version) {
+            console.log(`${inputLicensesKey}：名前またはバージョンがありません。`)
+            errorCount++
             continue
         }
-        const name = value.name
-        const version = value.version
-        let licenseText = value.licenseFile.match(/LICENSE/ig) || value.licenseFile.match(/LICENCE/ig) ? value.licenseText : null
-        if (!licenseText) {
-            let tmpLicenseType = value.licenses
-            if (tmpLicenseType === 'MIT') {
-                licenseText = generateLicenseText(mitTxt, value)
-                checkLicenseText(key, tmpLicenseType, licenseText)
-            } else if (tmpLicenseType === 'Apache-2.0') {
-                licenseText = generateLicenseText(apache20Txt, value)
-                checkLicenseText(key, tmpLicenseType, licenseText)
-            } else if (tmpLicenseType === 'ISC') {
-                licenseText = generateLicenseText(iscTxt, value)
-                checkLicenseText(key, tmpLicenseType, licenseText)
-            } else if (tmpLicenseType === 'BSD-2-Clause') {
-                licenseText = generateLicenseText(bsd2clauseTxt, value)
-                checkLicenseText(key, tmpLicenseType, licenseText)
-            } else if (tmpLicenseType === 'BSD-3-Clause') {
-                licenseText = generateLicenseText(bsd3clauseTxt, value)
-                checkLicenseText(key, tmpLicenseType, licenseText)
-            } else {
-                console.log(`${key}：ライセンスを確認してください`)
-            }
+        if (inputLicensesData.private) {
+            // privateの場合は出力対象にしない。
+            continue
         }
-        newLicensesJsonData[key] = {
+        const name = inputLicensesData.name
+        const version = inputLicensesData.version
+        const inputDataLicenseType = inputLicensesData.licenses
+        let licenseType = inputLicensesData.licenses
+        // 既知ライセンス情報が存在する場合は取得する。
+        let knownLicensesData = knownLicensesDictionary[name]
+        if (!knownLicensesData) {
+            // 既知ライセンス情報が見つからない場合、ライセンス種別を付加しもう一度チェックする。
+            knownLicensesData = knownLicensesDictionary[`${name}_${licenseType}`]
+        }
+        if (!!knownLicensesData && licenseType === knownLicensesData.licensesOrigin) {
+            // 既知ライセンス情報に存在し、かつlicensesOriginに記載された種別と一致する場合のみライセンス種別を上書きする。
+            // これにより、バージョンアップでライセンスが変更になっている場合に検出することが可能。
+            licenseType = knownLicensesData.licenses
+        }
+        let copyright = inputLicensesData.copyright
+        // ライセンスファイルのスペルが2種類存在するので、両方とも処理対象に含める。
+        let licenseText = inputLicensesData.licenseFile.match(/LICENSE/ig) || inputLicensesData.licenseFile.match(/LICENCE/ig) ? inputLicensesData.licenseText : null
+        switch (licenseType) {
+            case 'MIT':
+            case 'Apache-2.0':
+            case 'ISC':
+            case 'BSD-2-Clause':
+            case 'BSD-3-Clause':
+                if (!licenseText) {
+                    // 著作権情報が存在する場合はテンプレートと合成しライセンス条文を生成する。
+                    if (!!copyright) {
+                        licenseText = generateLicenseText(licenseTemplateTextDictionary[licenseType], copyright)
+                    } else {
+                        outputCopyrightError(inputLicensesKey, licenseType)
+                        errorCount++
+                        continue
+                    }
+                }
+                break
+            case 'KNOWN_LICENSE':
+                if (!!knownLicensesData.ignore) {
+                    // 出力不要の場合は無視する。
+                    continue
+                }
+                // 既知ライブラリ情報に記載されている場合で、定義しているもの以外のライセンス
+                licenseText = knownLicensesData.licenseText
+                if (!licenseText) {
+                    // ignoreの明示的な指定が無く、条文が存在しない場合はエラーとする。
+                    outputKnownLicenseTextError(inputLicensesKey)
+                    errorCount++
+                    continue
+                }
+            default:
+                outputLicenseError(inputLicensesKey, licenseType)
+                errorCount++
+                // チェックに引っかかったものは出力対象にしない。
+                continue
+        }
+        newLicensesDictionary[inputLicensesKey] = isOutputIndex ? {
+            index,
             name,
+            inputDataLicenseType,
+            version,
+            licenseText
+        } : {
+            name,
+            inputDataLicenseType,
             version,
             licenseText
         }
+        index++
     }
-    fs.writeFileSync(outputFile, JSON.stringify(newLicensesJsonData));
+    if (errorCount > 0 && !ignoreCheckError) {
+        throw new Error(`${errorCount}個の要確認事項が見つかりました。既知ライセンス情報ファイルにライセンス情報を追加する等の方法で要確認事項を解決し、再度実行してください。`)
+    }
+    if (isList) {
+        const newLicensesList = []
+        for (newLicensesKey in newLicensesDictionary) {
+            newLicensesList.push(newLicensesDictionary[newLicensesKey])
+        }
+        fs.writeFileSync(outputFile, JSON.stringify(newLicensesList));
+    } else {
+        fs.writeFileSync(outputFile, JSON.stringify(newLicensesDictionary));
+    }
+    if (errorCount > 0) {
+        outputConsoleWarning(`\r\n${errorCount}個の要確認事項が見つかりましたが、強制的にファイル出力を行いました。確認が必要なものは出力対象に含まれません。`)
+    }
 } catch (e) {
-    console.error('ファイルの読み込みに失敗しました')
+    if (e.message) {
+        outputConsoleWarning(`\r\n${e.message}`)
+    } else {
+        outputConsoleError('不明なエラーです。')
+    }
     process.exit(1)
 }
